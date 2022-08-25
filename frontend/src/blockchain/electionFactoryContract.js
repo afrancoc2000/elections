eb569ec710aa05bf0447591ea0fae0fafe22941d
Ana Maria Franco <afrancoc2000@gmail.com>
2022-08-25 07:45:30 -0500

    Se agrega compatibilidad con web3

--- frontend/src/blockchain/electionFactoryContract.js
+++ frontend/src/blockchain/electionFactoryContract.js
@@ -1,7 +1,9 @@
-import { web3 } from "./web3";
+// import { web3 } from "./web3";
 import electionFactoryAbi from "./electionFactoryAbi";
 import electionFactoryAddress from "./electionFactoryAddress";
+import Web3 from "web3";
 
 export function createContract() {
+  const web3 = new Web3(window.web3.provider);
   return new web3.eth.Contract(electionFactoryAbi, electionFactoryAddress);
 }
